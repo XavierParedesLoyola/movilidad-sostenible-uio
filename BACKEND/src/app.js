ccd7787511fc423a9c7c170a34c7c3fc676e5222
XavierParedesLoyola <bxparedes@puce.edu.ec>
2025-08-18 22:18:57 -0500

    Se agrega rutas de rutas y viajes

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -2,7 +2,11 @@
 require("dotenv").config(); // Para cargar variables de entorno
 const express = require("express"); // Framework para construir servidores web
 const cors = require("cors"); // Middleware permite solicitudes desde otros dominios
+
 const authRoutes = require("./routes/authRoutes"); // Importa las rutas
+const rutaRoutes = require("./routes/rutaRoutes");
+const viajeRoutes = require("./routes/viajeRoutes");
+
 const { getConnection } = require("./config/db"); // Importa la función de conexión a la BDD
 //#endregion
 
@@ -17,6 +21,9 @@ app.use(express.json()); // Procesa cuerpos JSON
 
 //#region Definición de rutas
 app.use("/api/auth", authRoutes);
+app.use("/api/rutas", rutaRoutes);
+app.use("/api/viajes", viajeRoutes);
+
 //#endregion
 
 const PORT = process.env.PORT || 3000;
